392390a6f16163c0d9162baa85ec98efcd026b01
nabeelibrahim13 <mnibrahim@cloud.neduet.edu.pk>
2023-09-25 23:11:19 -0700

    HTML Issue

--- app.py
+++ app.py
@@ -17,34 +17,39 @@ users = [
 
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template("signin.html")
 
-@app.route("/signup", methods=['POST'])
-def signup():
-    request_data = request.get_json()
-    new_user = {
-                'name': request_data['name'],
-                'email': request_data['email'],
-                'password': request_data['password']
-                }
-    users.append(new_user)
-    return jsonify({'users': users})
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+# @app.route("/signup", methods=['POST'])
+# def signup():
+#     request_data = request.get_json()
+#     new_user = {
+#                 'name': request_data['name'],
+#                 'email': request_data['email'],
+#                 'password': request_data['password']
+#                 }
+#     users.append(new_user)
+#     return jsonify({'users': users})
 
 
-@app.route('/signin/', methods=['POST'])
-def signin():
-    request_data = request.get_json()
-    for user in users:
-        if user['email'] == request_data['email'] and user['password'] == request_data['password']:
-            return jsonify({'message': 'success'})
-    return jsonify({'message': 'failure'})
+# @app.route('/signin', methods=['GET','POST'])
+# def signin():
+#     render_template("signin.html")
+#     request_data = request.get_json()
+#     for user in users:
+#         if user['email'] == request_data['email'] and user['password'] == request_data['password']:
+#             return jsonify({'message': 'success'})
+#     return jsonify({'message': 'failure'})
 
-@app.route('/showusers', methods=['GET'])
-def showusers():
-    return jsonify({'users': users})
+# @app.route('/showusers', methods=['GET'])
+# def showusers():
+#     return jsonify({'users': users})
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=4000)
