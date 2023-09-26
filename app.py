05f557dbe0ffda0498abb7bd2575a4ba83ff0d51
nabeelibrahim13 <mnibrahim@cloud.neduet.edu.pk>
2023-09-26 00:02:26 -0700

    HTML page working

--- app.py
+++ app.py
@@ -18,38 +18,36 @@ users = [
 
 @app.route('/')
 def index():
-    return render_template("signin.html")
+    return render_template("about.html")
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-# @app.route("/signup", methods=['POST'])
-# def signup():
-#     request_data = request.get_json()
-#     new_user = {
-#                 'name': request_data['name'],
-#                 'email': request_data['email'],
-#                 'password': request_data['password']
-#                 }
-#     users.append(new_user)
-#     return jsonify({'users': users})
+@app.route('/signup', methods=['GET','POST'])
+def signup(methods=['POST']):
+    return render_template("signup.html")
+    # request_data = request.get_json()
+    # new_user = {
+    #             'name': request_data['name'],
+    #             'email': request_data['email'],
+    #             'password': request_data['password']
+    #             }
+    # users.append(new_user)
+    return render_template("signup.html")
 
 
-# @app.route('/signin', methods=['GET','POST'])
-# def signin():
-#     render_template("signin.html")
-#     request_data = request.get_json()
-#     for user in users:
-#         if user['email'] == request_data['email'] and user['password'] == request_data['password']:
-#             return jsonify({'message': 'success'})
-#     return jsonify({'message': 'failure'})
+@app.route('/signin', methods=['GET','POST'])
+def signin():
+    return render_template('signin.html')
+    # render_template("signin.html")
+    # request_data = request.get_json()
+    # for user in users:
+    #     if user['email'] == request_data['email'] and user['password'] == request_data['password']:
+    #         return jsonify({'message': 'success'})
+    # return jsonify({'message': 'failure'})
 
-# @app.route('/showusers', methods=['GET'])
-# def showusers():
-#     return jsonify({'users': users})
+@app.route('/showusers', methods=['GET'])
+def showusers():
+    return jsonify({'users': users})
 
 
 
 if __name__ == "__main__":
-    app.run(port=4000)
+    app.run(debug=True, port = 4001)
